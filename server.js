4a1eeb0d4387de9c67eb315919fdece6168c15c5
SleeplessEmpress <136986655+SleeplessEmpress@users.noreply.github.com>
2023-10-09 14:25:36 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const adyenEncrypt = require('node-adyen-encrypt')(25);
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/adyenEncrypt', function (req, res) {
+  try {
+    const data = req.body;
+    const card = data.card;
+    const encryptionKey = data.encryptionKey;
+    const [cardNumber, expiryMonth, expiryYear, securityCode] = card.split("|");
+    const generationtime = new Date().toISOString();
+
+    const adyenKey = encryptionKey;
+
+    const options = {};
+
+    const cardData = {
+      number: cardNumber,
+      generationtime: generationtime
+    };
+
+    const cardData1 = {
+      expiryMonth: expiryMonth,
+      generationtime: generationtime
+    };
+
+    const cardData2 = {
+      expiryYear: expiryYear,
+      generationtime: generationtime
+    };
+
+    const cardData3 = {
+      cvc: securityCode,
+      generationtime: generationtime
+    };
+
+    const cseInstance = adyenEncrypt.createEncryption(adyenKey, options);
+
+    const encryptedCardNumber = cseInstance.encrypt(cardData);
+    const encryptedExpiryMonth = cseInstance.encrypt(cardData1);
+    const encryptedExpiryYear = cseInstance.encrypt(cardData2);
+    const encryptedSecurityCode = cseInstance.encrypt(cardData3);
+
+    res.json({
+      'encryptedCardNumber': encryptedCardNumber,
+      'encryptedExpiryMonth': encryptedExpiryMonth,
+      'encryptedExpiryYear': encryptedExpiryYear,
+      'encryptedSecurityCode': encryptedSecurityCode,
+      'Encrypted By': '@RailgunMisaka'
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred during encryption.' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
