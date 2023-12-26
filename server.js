294498e55f44019d5cc2d0412e7f9d8006301f99
SleeplessEmpress <136986655+SleeplessEmpress@users.noreply.github.com>
2023-12-27 02:51:02 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +12,7 @@ app.post('/adyenEncrypt', function (req, res) {
     const version = data.version;
     const card = data.card;
     const encryptionKey = data.encryptionKey;
-    const [cardNumber, expiryMonth, expiryYear, securityCode] = card.split("|");
+    const [cardNumber, expiryMonth, expiryYear, cvc] = card.split("|");
     const generationtime = new Date().toISOString();
 
     const adyenEncrypt = require('node-adyen-encrypt')(version);
@@ -36,7 +36,7 @@ app.post('/adyenEncrypt', function (req, res) {
     };
 
     const cardData3 = {
-      cvc: securityCode,
+      cvc: cvc,
       generationtime: generationtime
     };
 
@@ -57,6 +57,52 @@ app.post('/adyenEncrypt', function (req, res) {
   } catch (error) {
     res.status(500).json({ error: 'An error occurred during encryption.' });
   }
+});
+
+app.post('/adyenSingleEncryption', function (req, res) {
+  try {
+    const data = req.body;
+    const version = data.version;
+    const card = data.card;
+    const encryptionKey = data.encryptionKey;
+    const [number, expiryMonth, expiryYear, cvc] = card.split("|");
+    const generationtime = new Date().toISOString();
+
+    function addSpacesToCardNumber(number) {
+      const cardNumberWithoutSpaces = number.replace(/\s/g, '');
+      const cardNumber = cardNumberWithoutSpaces.replace(/(.{4})/g, '$1 ');
+      return cardNumber.trim();
+    }
+
+    const cardNumber = addSpacesToCardNumber(number);
+    const adyenEncrypt = require('node-adyen-encrypt')(version);
+    const adyenKey = encryptionKey;
+
+    const options = {};
+
+    const cardData = {
+      number : cardNumber,
+      cvc : cvc,
+      expiryMonth : expiryMonth,
+      expiryYear : expiryYear,
+      generationtime : generationtime
+    };
+
+    const cseInstance = adyenEncrypt.createEncryption(adyenKey, options);
+    const encryptedData = cseInstance.encrypt(cardData);
+
+    res.json({
+      'encryptedData': encryptedData,
+      'cardNumber': cardNumber,
+      'Encrypted By': '@RailgunMisaka'
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred during encryption.' });
+  }
+});
+
+app.get('/', function (req, res) {
+  res.send('Please replace your request\'s method from GET to POST.');
 });
 
 app.listen(port, () => {
